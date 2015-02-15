a8b8e38dc4064c66e77ee3dc1e7e5e239ebe5a0f
Dawid Zięba <dzieba@pgs-soft.com>
2015-02-15 20:27:40 +0100

    Dependencies in maven, configuration spring and hibernate

---
+++ src/main/java/org/prz/java/pm/core/entity/Role.java
@@ -0,0 +1,34 @@
+package org.prz.java.pm.core.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "ROLES")
+public class Role extends BaseEntity{
+
+    @Column
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
---
+++ src/main/java/org/prz/java/pm/core/entity/User.java
@@ -0,0 +1,65 @@
+package org.prz.java.pm.core.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="USERS")
+public class User extends BaseEntity{
+
+    @Column(name="first_name", nullable = false)
+    private String firstName;
+
+    @Column(name="last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany
+    @JoinColumn
+    private List<Role> roles;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+}
